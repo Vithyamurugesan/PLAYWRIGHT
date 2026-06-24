8d35f607315a98db9a74f3b9a710c2a04f4ec0d6
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-24 21:01:39 +0530

    Report added

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -35,7 +38,7 @@ export default defineConfig({
     headless:false,
     screenshot:'only-on-failure',
     video:'retain-on-failure',
-    trace: 'on-first-retry'
+    trace: 'on'
   },
 
   /* Configure projects for major browsers */
