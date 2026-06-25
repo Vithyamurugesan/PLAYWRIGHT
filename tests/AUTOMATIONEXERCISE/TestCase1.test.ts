948cb08a69afc7c6f0c1378cca09d6ef01a35648
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-25 17:19:28 +0530

    Testcase1 implemented

--- tests/AUTOMATIONEXERCISE/TestCase1.test.ts
+++ tests/AUTOMATIONEXERCISE/TestCase1.test.ts
@@ -9,17 +9,28 @@ test("Resgister User",async()=>{
 
     await page.goto("https://automationexercise.com/login");
     await page.getByPlaceholder('Name').fill('Vithyak');
-    await page.locator('[data-qa="signup-email"]').fill('vithyak143@gmail.com');
+    await page.locator('[data-qa="signup-email"]').fill('vithyak1431435464@gmail.com');
     await page.getByRole('button',{name:'Signup'}).click();
-
-    await expect(page.getByText('Enter Account Information')).toBeVisible();
-
     
 
+    await expect(page.getByText('Enter Account Information')).toBeVisible();
+    await page.locator('#id_gender2').check();
 
+    await page.locator('[data-qa="password"]').fill('12345678');
+    await page.locator('#newsletter').check();
+    await page.locator('#optin').check();
 
+    await page.locator('#first_name').fill('vithyaK');
+    await page.locator('[data-qa="last_name"]').fill('M');
+    await page.locator('[data-qa="company"]').fill('xyz');
+    await page.locator('[data-qa="address"]').fill('pnp');
+    await page.locator('#country').selectOption('India');
+    await page.locator('[data-qa="state"]').fill('tamilnadu');
+    await page.locator('[data-qa="city"]').fill('salem');
+    await page.locator('[data-qa="zipcode"]').fill('636109');
+    await page.locator('[data-qa="mobile_number"]').fill('9876543219');
 
-
+    await page.getByRole('button',{name:'Create Account'}).click();
 
 }
 )
