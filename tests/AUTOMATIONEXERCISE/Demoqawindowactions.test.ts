a57796ceddadca734ae3414db7d1a6d5d5e02268
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-26 16:55:41 +0530

    Iterate windows

---
+++ tests/AUTOMATIONEXERCISE/Demoqawindowactions.test.ts
@@ -0,0 +1,40 @@
+import {test,expect} from "@playwright/test";
+
+test("Iterate windows",async({page,context})=>{
+    await page.goto("https://demoqa.com/browser-windows")
+
+    //open new tab
+    const [tab]=await Promise.all([
+        context.waitForEvent("page"),
+        page.locator("#tabButton").click(),
+    ]);
+
+    //wait for the new tab to load
+    await tab.waitForLoadState();
+
+    //open new window
+    const [window]=await Promise.all([
+        context.waitForEvent("page"),
+        page.locator("#windowButton").click(),
+    ]);
+
+    await tab.waitForLoadState();
+
+    const pages=context.pages();
+
+    console.log("number of pages:",pages.length);
+
+    for (const p of pages){
+        console.log("-------------");
+        console.log("URL:",p.url());
+        console.log("title:",await p.title());
+    }
+});
+
+
+
+
+
+
+
+   
