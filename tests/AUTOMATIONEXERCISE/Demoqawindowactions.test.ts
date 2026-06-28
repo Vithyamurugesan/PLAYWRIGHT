da5b1f062a09d25af6ab2b427b009320254e367c
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-28 21:41:13 +0530

    window actions

--- tests/AUTOMATIONEXERCISE/Demoqawindowactions.test.ts
+++ tests/AUTOMATIONEXERCISE/Demoqawindowactions.test.ts
@@ -31,6 +31,34 @@ test("Iterate windows",async({page,context})=>{
     }
 });
 
+test("Iterate window attribute",async({page,context})=>{
+     await page.goto("https://demoqa.com/browser-windows");
+     const [tab]=await Promise.all([context.waitForEvent("page"),
+        page.locator("#tabButton").click()]);
+        
+        await tab.waitForLoadState();
+       
+       
+       
+        const [window]=await Promise.all([
+            context.waitForEvent("page"),
+            page.locator("#windowButton").click()
+        ]);
+        await window.waitForLoadState();
+
+        //Get all pages
+        const pages=context.pages();
+        console.log("Total Pages opened:",pages.length);
+
+        for(const p of pages){
+            console.log("URL:",p.url());
+            if(p.url().includes("sample")){
+                const text=await p.locator("#sampleHeading").textContent();
+                console.log("Sample Page Heading:",text);
+            }
+        }
+});
+
 
 
 
@@ -38,3 +66,4 @@ test("Iterate windows",async({page,context})=>{
 
 
    
+
